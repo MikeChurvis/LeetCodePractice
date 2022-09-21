dd8e7232ec8ea619430bc232462a3784b74597e5
Mike Churvis <mikechurvis@gmail.com>
2022-09-20 21:40:58 -0400

    Add snippets.py

---
+++ snippets.py
@@ -0,0 +1,20 @@
+if __name__ == "__main__":
+    def main():
+        example_data = [
+            # (input, expected_output),
+            # ...
+        ]
+
+        solver_fn = None # = Solution().your_method_here
+
+        for datum in example_data:
+            input_data, expected_output = datum
+            actual_output = solver_fn(input_data)
+
+            print(f"Input:\t{input_data}")
+            print(f"Expect:\t{expected_output}")
+            print(f"Actual:\t{actual_output}")
+            print()
+
+
+    main()
